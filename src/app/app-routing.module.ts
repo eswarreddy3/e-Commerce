ad31ba390daf52f4afcacd07b5d1aa4c07ffbc01
Venkata Sai Eswar Reddy Pagadala <109944287+eswarreddy3@users.noreply.github.com>
2025-03-07 08:26:55 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],  // Keep clean URLs
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
